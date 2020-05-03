0ef25a1eb28c58625652deeb2d924992635431aa
Andrei Zavatski <megaman9919@gmail.com>
2020-05-03 10:29:45 +0300

    Add test track

--- IWBTM.Game/IWBTMGame.cs
+++ IWBTM.Game/IWBTMGame.cs
@@ -1,11 +1,14 @@
 ﻿using osu.Framework.Allocation;
 using IWBTM.Game.Playfield;
 using osuTK;
+using osu.Framework.Audio.Track;
 
 namespace IWBTM.Game
 {
     public class IWBTMGame : IWBTMGameBase
     {
+        private Track track;
+
         [BackgroundDependencyLoader]
         private void load()
         {
@@ -13,6 +16,15 @@ namespace IWBTM.Game
             {
                 Child = new DefaultPlayfield()
             });
+
+            track = Audio.Tracks.Get("Ghost Rule");
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            track.Start();
         }
     }
 }
