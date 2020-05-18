7b69722523afa95057f14808cf02ebbb3923b6af
Andrei Zavatski <megaman9919@gmail.com>
2020-05-18 18:45:46 +0300

    Add position tracking to TestGameplayScreen

--- IWBTM.Game/Screens/TestGameplayScreen.cs
+++ IWBTM.Game/Screens/TestGameplayScreen.cs
@@ -1,6 +1,7 @@
 ﻿using IWBTM.Game.Rooms;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osuTK;
 
@@ -8,6 +9,9 @@ namespace IWBTM.Game.Screens
 {
     public class TestGameplayScreen : GameplayScreen
     {
+        private readonly SpriteText xPosition;
+        private readonly SpriteText yPosition;
+
         public TestGameplayScreen(Room room)
             : base(room)
         {
@@ -26,10 +30,28 @@ namespace IWBTM.Game.Screens
                         {
                             LabelText = "Show hitbox",
                             Current = Playfield.Player.ShowHitbox
-                        }
+                        },
+                        xPosition = new SpriteText(),
+                        yPosition = new SpriteText(),
                     }
                 }
             });
+        }
+
+        private Vector2 lastPlayerPosition;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            var playerPosition = Playfield.Player.PlayerPosition();
+
+            if (playerPosition != lastPlayerPosition)
+            {
+                lastPlayerPosition = playerPosition;
+                xPosition.Text = $"X: {playerPosition.X}";
+                yPosition.Text = $"Y: {playerPosition.Y}";
+            }
         }
     }
 }
