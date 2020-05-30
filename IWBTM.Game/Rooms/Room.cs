76da70000692f6162d01b1b78cb6248c51165c69
Andrei Zavatski <megaman9919@gmail.com>
2020-05-30 20:55:19 +0300

    Add custom audio property to Room

--- IWBTM.Game/Rooms/Room.cs
+++ IWBTM.Game/Rooms/Room.cs
@@ -6,6 +6,8 @@ namespace IWBTM.Game.Rooms
     {
         public string Music { get; set; }
 
+        public bool CustomAudio { get; set; }
+
         public List<Tile> Tiles { get; set; }
     }
 }
