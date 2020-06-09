fc03bed95505bd89defe4086c4623dd8cb29064d
Andrei Zavatski <megaman9919@gmail.com>
2020-06-09 16:11:23 +0300

    Bump version

--- IWBTM.Game/Screens/MainMenuScreen.cs
+++ IWBTM.Game/Screens/MainMenuScreen.cs
@@ -51,7 +51,7 @@ namespace IWBTM.Game.Screens
                 {
                     Anchor = Anchor.BottomRight,
                     Origin = Anchor.BottomRight,
-                    Text = "v.0.7.2",
+                    Text = "v.0.7.3",
                     Margin = new MarginPadding(10)
                 }
             });
